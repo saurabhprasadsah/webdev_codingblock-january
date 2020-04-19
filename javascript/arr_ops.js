736c42a2590a2fa078a74b43d1fd096ff39d8c31
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-19 21:34:56 +0530

    array

---
+++ javascript/arr_ops.js
@@ -0,0 +1,128 @@
+let arr1=[,23,45,5,5,5654,545554545,5,43434]
+undefined
+arr1
+Array(9) [ <1 empty slot>, 23, 45, 5, 5, 5654, 545554545, 5, 43434 ]
+
+arr1.slice(4,5)
+Array [ 5 ]
+
+arr1
+Array(9) [ <1 empty slot>, 23, 45, 5, 5, 5654, 545554545, 5, 43434 ]
+arr = [5]
+Array [ 5 ]
+
+arr =[4]
+Array [ 4 ]
+
+arr [5]=10
+10
+arr
+(6) […]
+​
+0: 4
+​
+5: 10
+​
+length: 6
+​
+<prototype>: Array []
+
+Array[1,2,3,4,5,6,7,8]
+undefined
+arr[15]=17
+17
+arr
+(16) […]
+​
+0: 4
+​
+5: 10
+​
+15: 17
+​
+length: 16
+​
+<prototype>: []
+​​
+concat: function concat()
+​​
+constructor: function Array()
+​​
+copyWithin: function copyWithin()
+​​
+entries: function entries()
+​​
+every: function every()
+​​
+fill: fill()
+​​​
+length: 1
+​​​
+name: "fill"
+​​​
+<prototype>: function ()
+​​
+filter: function filter()
+​​
+find: function find()
+​​
+findIndex: function findIndex()
+​​
+flat: function flat()
+​​
+flatMap: function flatMap()
+​​
+forEach: function forEach()
+​​
+includes: function includes()
+​​
+indexOf: function indexOf()
+​​
+join: function join()
+​​
+keys: function keys()
+​​
+lastIndexOf: function lastIndexOf()
+​​
+length: 0
+​​
+map: function map()
+​​
+pop: function pop()
+​​
+push: function push()
+​​
+reduce: function reduce()
+​​
+reduceRight: function reduceRight()
+​​
+reverse: function reverse()
+​​
+shift: function shift()
+​​
+slice: function slice()
+​​
+some: function some()
+​​
+sort: function sort()
+​​
+splice: function splice()
+​​
+toLocaleString: function toLocaleString()
+​​
+toString: function toString()
+​​
+unshift: function unshift()
+​​
+values: function values()
+​​
+Symbol(Symbol.iterator): function values()
+​​
+Symbol(Symbol.unscopables): Object { copyWithin: true, entries: true, fill: true, … }
+​​
+<prototype>: Object { … }
+
+arr.length
+16
+
+​
