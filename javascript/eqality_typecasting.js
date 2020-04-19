6472b3e577216e0b216bfcd7cd7b88550c4dc973
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-19 17:55:27 +0530

    typecasting

---
+++ javascript/eqality_typecasting.js
@@ -0,0 +1,7 @@
++"1"
+console.log(1)//type casting the number
+"1"
+console.log("1")  //type casting the number
+
+
+
---
+++ javascript/equality.js
@@ -0,0 +1,5 @@
+1 == "1"
+true /*because equal operator use the equal the both the side */
+1 === "1"
+false /* because striped eqality  operator use the both the side and not equal the both the side*/
+/* type and value is same then all the eqal operator is use */
