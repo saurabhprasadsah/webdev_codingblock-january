e32c69bc6c93edfe7d27abc605974ceb7d2c3fa9
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-20 17:34:26 +0530

    strings

---
+++ javascript/strings.js
@@ -0,0 +1,28 @@
+///strigs
+let str="this is string"
+console.log(str)
+let str="thias is a saurabh string"
+undefined
+console.log(str)
+let str="this is a saurabh "string"
+SyntaxError: unexpected token: identifier
+debugger eval code:1:28
+console.log(str)
+undefined
+thias is a saurabh string debugger eval code:1:9
+let str="this is a saurabh\"string"
+console.log(str)
+SyntaxError: redeclaration of let str
+debugger eval code:1:1
+console.log(str)
+undefined
+thias is a saurabh string
+let longstr = "this is a \
+saurabh"
+undefined
+console.log(longstr)
+undefined
+this is a saurabh
+console.log(longstr.length)
+undefined
+17
