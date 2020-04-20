0d7cd89eebb5fc1f7ed4df42b54d08f9f7083061
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-20 18:01:38 +0530

    arrays

---
+++ mediaqueries/Arrays.js
@@ -0,0 +1,17 @@
+let arr=[1,23,4,45]
+undefined
+console.log(arr)
+undefined
+Array(4) [ 1, 23, 4, 45 ]
+
+console.log(arr[1])
+undefined
+23 
+console.log(arr[3])
+undefined
+45 
+console.log(arr[5])
+console.log(arr[-1])
+console.log(arr[2])
+undefined
+4
