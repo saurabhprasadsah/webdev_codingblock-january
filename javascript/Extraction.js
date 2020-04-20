da679913dff6b1af358de4672fb56d00c0010313
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-20 17:56:38 +0530

    exctraction

---
+++ javascript/Extraction.js
@@ -0,0 +1,11 @@
+let mainstr="this is a main string"
+undefined
+let mainStr="this is a main string"
+undefined
+let smallstr=mainStr.slice(2,3)
+undefined
+console.lo
+undefined
+console.log(smallstr)
+undefined
+i
