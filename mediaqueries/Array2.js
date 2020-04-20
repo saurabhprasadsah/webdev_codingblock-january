77dc76d71bd838b078ac37ec665a50cfd24167d6
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-20 18:16:41 +0530

    array2js

---
+++ mediaqueries/Array2.js
@@ -0,0 +1,23 @@
+///Array2 javascript for removing for the element 
+let fruits= ["Apple","banana"]
+undefined
+console.log(fruits)
+undefined
+Array [ "Apple", "banana" ]
+let fruits= ["Apple","banana"]
+undefined
+console.log(fruits)
+undefined
+Array [ "Apple", "banana" ]
+debugger eval code:1:9
+///push and pop operation
+fruits.push("melon")
+3
+console.log(fruits)
+undefined
+Array(3) [ "Apple", "banana", "melon" ]
+fruits.pop()
+"melon"
+console.log(fruits)
+undefined
+Array [ "Apple", "banana" ]
