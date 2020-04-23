9fbbcb759df2724fdd2ba9da317127baf69182a4
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-23 19:35:26 +0530

    array3

---
+++ javascript/Array3rd.js
@@ -0,0 +1,7 @@
+let notes = ['de','re','ge','hg','yu','jh']
+//slice
+//console.log(notes.slice(4,6))
+
+//splice
+notes.splice(4)
+console.log(notes)
--- javascript/Extraction.js
+++ javascript/Extraction.js
@@ -8,4 +8,5 @@ console.lo
 undefined
 console.log(smallstr)
 undefined
-i
+
+
