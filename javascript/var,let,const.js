2805d03b390152d1093fb6e8e167e91a13a7dfbd
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-29 18:06:27 +0530

    varconast

---
+++ javascript/var,let,const.js
@@ -0,0 +1,31 @@
+// // var a = 10;
+// // a++;
+// // console.log(a)
+
+// // let b = 30;
+// // b++;
+// // console.log(b);
+
+// // const c =40;
+// // c++;
+// // console.log(c);
+
+
+// const obj = {
+//     k1:'abd',
+//     k2: 200,
+//     k3: false
+// };
+
+// obj.k4 = 123;
+// console.log(obj.k4);
+
+// delete obj.k1;
+// console.log(obj.k1);
+
+// const arr = [1,2,3];
+// arr.push(4);
+// console.log(arr);
+
+
+
