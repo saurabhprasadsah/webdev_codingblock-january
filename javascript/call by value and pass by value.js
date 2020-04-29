b336c2d9b2471930b02a108aaedb2198e7a32124
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-29 17:27:01 +0530

    call by value

---
+++ javascript/call by value and pass by value.js
@@ -0,0 +1,17 @@
+var quidditchWinner = "Harry potter";
+
+function changeWinner(winner){
+    Console.log("original winner="+winner);
+
+    winner= "Darco marflo"
+
+    console.log("changed winner="+winner);
+
+}
+console.log("this winner was"+quidditchWinner);
+
+changeWinner(quidditchWinner);
+ console.log("Now the winner is "+quidditchWinner);
+
+
+
