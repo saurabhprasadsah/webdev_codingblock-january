beaaf9b20819c80c61accfc7782ad57b16779cbb
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-29 21:32:30 +0530

    callstack

---
+++ javascript/call stack.js
@@ -0,0 +1,23 @@
+function ceo (){
+    console.log("lets the make the dent  universe")
+    cto()
+}
+function cto (){
+    console.log("lets make changing product")
+    vpeengg()
+}
+
+function vpeengg(){
+    console.log("lets make a javascript framework")
+    techlead()
+}
+function techlead(){
+    console.log("lets port angular to typescript")
+    devloper()
+    
+}
+function devloper() {
+    console.log("lets copy from the stackoverflow side")
+
+}
+ceo();
