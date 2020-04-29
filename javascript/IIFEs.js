24a68006fb495a8216a511239cb754ff3ea1741c
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-29 17:08:37 +0530

    IIFES javascript

---
+++ javascript/IIFEs.js
@@ -0,0 +1,28 @@
+// //iife - immediatate invoked function
+
+// (function sayhello (){
+ 
+//     var a = 10;
+//     var b = a/5;
+//     console.log(b)
+
+
+//     ///console.log("hello");
+// }) ();
+
+// ///console.log(a)
+
+// ///sayhello()
+
+// var a = 10;
+// if(true){
+//     console.log(10)
+// }
+ 
+
+function domath () {
+    console.log("3^4 =" + Math.pow(3,4));
+    console.log("4^3 =" + Math.pow(4,3));
+
+}
+domath();
