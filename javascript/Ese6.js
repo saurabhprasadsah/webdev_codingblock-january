363e0b57dcc62e44d7feae091a1ba36f4da68a6a
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-04-29 09:24:02 +0530

    es6

--- javascript/Ese6.js
+++ javascript/Ese6.js
@@ -1,14 +1,28 @@
-function fakedownload (done) {
-    setTimeout (function (){
-    let downloadedData = "some data will be rapped of the interwebs"
-    done (downloadedData)
+// function fakedownload (done) {
+//     setTimeout (function (){
+//     let downloadedData = "some data will be rapped of the interwebs"
+//     done (downloadedData)
 
 
 
-    }, 1000)
+//     }, 1000)
+// }
+// fakedownload(function(data){
+//     console.log("which downloaded the file from this data --->")
+//     console.log(data)
+// })
+
+function fakedownloadPromise(){
+    return new Promise(function (resolve,reject){
+        setTimeout(function (){
+            let downloadedData=" some data is downloaded from net"
+            resolve (downloadedData)
+
+        },1000)
+    })
 }
-fakedownload(function(data){
-    console.log("which downloaded the file from this data --->")
+fakedownloadPromise(). then(function (data){
+    console.log("the data is downloaded form --->")
     console.log(data)
-})
 
+})
