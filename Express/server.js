0b721d0a8ff6e42fbc10fecb546d1b536fe8d127
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-05-13 20:27:52 +0530

    express

--- Express/server.js
+++ Express/server.js
@@ -7,6 +7,14 @@ const app = express()
 app.get('/',(req, res)=>{
     res.send('<h1 style="color: red;"> Hello world</h1>')
 })
+app.get('/greet',(req, res)=>{
+
+    console.log(req.headers)
+    res.send('Good morning')
+})
+
+
+
 
 app.listen(4444,()=>{
     console.log('server start on http://localhost:4444')
