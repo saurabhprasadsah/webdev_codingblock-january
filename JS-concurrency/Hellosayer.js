568783664f5d9b5b7e10a684ec5430e30f6c2d49
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-05-14 16:47:24 +0530

    jsconcurrency

--- JS-concurrency/Hellosayer.js
+++ JS-concurrency/Hellosayer.js
@@ -3,6 +3,11 @@ function hellosayer(times,name){
     let loopId = setInterval(()=>{
         count++
         console.log('Hello'+name)
-        if (count
-    })
+        if (count = times){
+            clearInterval(loopId)
+        }
+    }, 100)
 }
+
+hellosayer(3,'saurabh')
+hellosayer(5,'Manish')
