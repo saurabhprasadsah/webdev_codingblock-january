d68c911b9bf584d2e786abc1ab379a8dbaa4d2e0
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-05-16 18:34:20 +0530

    node_js_intro

---
+++ browser_codesharing/lib.js
@@ -0,0 +1,8 @@
+window.awesomeFunction= function(){
+    console.log("omg! this is awesomefunction")
+}
+
+
+module.exports={
+    awesomeFunction
+}
---
+++ browser_codesharing/script.js
@@ -0,0 +1,8 @@
+//awesomeFunction();
+
+const lib = require('./lib.js')
+
+
+console.log(lib)
+
+lib.awesomeFunction()
