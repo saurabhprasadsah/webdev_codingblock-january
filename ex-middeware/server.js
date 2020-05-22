ee0de166dd13d04889fe03108b1de4e2e7bcf81b
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2020-05-22 15:39:07 +0530

    middleware
    Signed-off-by: saurabhprasadsah <kumarsaurabh2910@gmail.com>

--- ex-middeware/server.js
+++ ex-middeware/server.js
@@ -1,3 +1,35 @@
 const express = require('express')
 
 const app = express()
+
+
+function m1 (req,res,next){
+    console.log('Rinnig middleware 1')
+    next()
+}
+function m2 (req,res,next){
+    console.log('Rinnig middleware 1')
+    next()
+}
+function m3 (req,res,next){
+    console.log('Rinnig middleware 1')
+    next()
+}
+function m4 (req,res,next){
+    console.log('Rinnig middleware 1')
+    next()
+}
+function m5 (req,res,next){
+    console.log('Rinnig middleware 1')
+    next()
+}
+
+app.get('/hello',m2,m3(req,res)=>{
+    res.send('Hello world')
+})
+
+
+
+app.listen(4343,()=>{
+    console.log('server on the http://localhost:4343')
+})
