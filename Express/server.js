582ecec47df316ab39461447e9a85548dc79889d
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2021-11-13 20:55:18 +0530

    express

--- Express/server.js
+++ Express/server.js
@@ -1,7 +1,5 @@
 const express = require('express')
-
 const app = express()
-
 
 app.get('/',(req, res)=>{
     res.send('<h1 style="color: red;"> Hello world</h1>')
@@ -13,15 +11,10 @@ app.get('/greet',(req, res)=>{
 })
 app.get('/forms',(req,res)=>{
     res.send('C:\Users\saurabh kumar\Desktop\Web dev class\webdev_codingblock-january\Express\forms\forms.html')
-       
-
 })
-
-
-
 app.listen(4444,()=>{
     console.log('server start on http://localhost:4444')
 })
 
-
+//express js
 //port(4444)../port vaulue
