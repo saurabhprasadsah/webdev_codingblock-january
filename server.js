b89d756d90bd10200de2bbb6c081be4922fd21fa
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2021-11-23 20:36:42 +0530

    ajax  get call
    Access ajax call

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-var rest = require('restler');
- 
-rest.get('http://localhost:5984/mycompany/1ba2c5b5d902c63d6ec8b6b67600631d', {
-"username": "",
-"password": ""
+var express = require('express');
+var path = require('path');
+var request = require('ajax-request');
+var router = express.Router();
 
-}).on('complete', function(result) {
-  if (result instanceof Error) {
-    console.log('Error:', result.message);
-  } else {
-    console.log(result);
-  }
+router.get('/', function (req, res) {
+    res.sendFile('/html/index.html');
+
 });
+function run() {
+
+    // Creating Our XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+    // Making our connection 
+    var url = 'http://localhost:5984/mycompany/' + id;
+    xhr.open("GET", url, true, "saurabh", "rajukumar@123");
+    // function execute after request is successful
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    }
+    // Sending our request
+    xhr.send();
+}
+run();
+
+console.log("hello ajax");
