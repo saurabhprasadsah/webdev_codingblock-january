921c23918eaffc23acec637a2f3e3088e7a51749
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2021-11-23 20:42:26 +0530

    server.js

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-var express = require('express');
-var path = require('path');
-var request = require('ajax-request');
-var router = express.Router();
+const express = require('express')
+const fs = require("fs")
+const app = express()
 
-router.get('/', function (req, res) {
-    res.sendFile('/html/index.html');
+app.get('/', (req,res)=>{
 
-});
+    fs.readFile('app.json','utf-8', function(err,data){
+        console.log(data);
+        
+        res.send(data)
+    });
 
+    
+    
+})
+app.get('/get', (req,res)=>{
+    res.send('get request')
+})
 
-// simple get call without jquery
-function run() {
-
-    // Creating Our XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-    // Making our connection 
-    var url = 'http://localhost:5984/mycompany/' + id;
-    xhr.open("GET", url, true, "usename", "password");
-    // function execute after request is successful
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-        }
-    }
-    // Sending our request
-    xhr.send();
-}
-run();
-
-console.log("hello ajax");
+app.listen(3233)
