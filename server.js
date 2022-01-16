eaa8141df0bc2e1369476e482aa14d208cb23059
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2022-01-16 18:42:48 +0530

    express

--- server.js
+++ server.js
@@ -3,18 +3,14 @@ const fs = require("fs")
 const app = express()
 
 app.get('/', (req,res)=>{
-
     fs.readFile('app.json','utf-8', function(err,data){
-        console.log(data);
-        
+        console.log(data);    
         res.send(data)
-    });
-
-    
-    
+    });    
 })
 app.get('/get', (req,res)=>{
     res.send('get request')
 })
 
 app.listen(3233)
+//console.log("helllo world")
